16b1663d598dfd69e77c18c6e8dd198180fd19df
will <willbee8888@gmail.com>
2024-09-16 20:13:05 +0100

    Who knows

--- EsWinApp/obj/Debug/net8.0-windows/EsWinApp.AssemblyInfo.cs
+++ EsWinApp/obj/Debug/net8.0-windows/EsWinApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsWinApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbafdaa8a78c996fe0bf4ac3d64a3791befd8406")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsWinApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsWinApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
